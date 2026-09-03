3111a72d2760e25e0a5c2ad88e88b408f9019364
Mullaguri Umesh Chandra <umeshchandramullaguri@gmail.com>
2026-09-03 11:11:13 +0530

    Allow all hosts in vite preview and server configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
@@ -14,5 +15,10 @@ export default defineConfig({
         secure: false,
       }
     }
+  },
+  preview: {
+    port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: true,
   }
 })
